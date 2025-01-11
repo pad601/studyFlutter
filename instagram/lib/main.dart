cf46380b158f99e5adc1bd4c76387caa52433952
pad601 <myhan601@naver.com>
2025-01-11 20:59:25 +0900

    instagram feed complete

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,94 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.camera, color: Colors.black),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(CupertinoIcons.paperplane, color: Colors.black),
+            onPressed: () {},
+          )
+        ],
+        title: Image.asset(
+          'assets/logo.png',
+          height: 32,
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // 이미지
+          Image.network(
+            "https://i.ibb.co/YjjLCS7/cat.png",
+            height: 400,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Row(
+            children: [
+              IconButton(
+                icon: Icon(CupertinoIcons.heart, color: Colors.black),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(CupertinoIcons.chat_bubble, color: Colors.black),
+                onPressed: () {},
+              ),
+              Spacer(),
+              IconButton(
+                icon: Icon(CupertinoIcons.bookmark, color: Colors.black),
+                onPressed: () {},
+              ),
+            ],
+          ),
+
+          // 좋아요
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "2 likes",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          // 설명
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "My cat is docile even when bathed. I put a duck on his head in the wick and he's staring at me. Isn't it so cute??",
+            ),
+          ),
+
+          // 날짜
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "FEBURARY 6",
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
